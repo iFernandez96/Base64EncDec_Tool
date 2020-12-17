17efcff1c743d6199d60a25ef58998be473e16ee
iFernandez96 <51539200+iFernandez96@users.noreply.github.com>
2020-12-17 10:01:51 -0800

    Updated to add TCP Server Connection

--- Base64EncDec_Tool.c
+++ Base64EncDec_Tool.c
@@ -3,7 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "Base64.h"
+#include <unistd.h> 
+#include <sys/socket.h>
+#include <netinet/in.h> 
+
+#include "Base64.h" // Thanks to 
+
+#define PORT 9999
+
 
 
 const char OutputFileForDecoded[] = "DecodedOutput.txt"; 
@@ -25,70 +32,102 @@ void PrintUseCase()
 
 int main( int argc, char *argv[] )  {
 
-   if( argc == 2 )
+
+   int server_fd, new_socket, valread; 
+	struct sockaddr_in address; 
+	int opt = 1; 
+	int addrlen = sizeof(address); 
+   char buffer[2048];
+
+   // Creating socket file descriptor 
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+	{ 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+   
+	// Forcefully attaching socket to the port 9999 
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+												&opt, sizeof(opt))) 
+	{ 
+		perror("setsockopt"); 
+		exit(EXIT_FAILURE); 
+	} 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; 
+	address.sin_port = htons( PORT ); 
+	
+	// Forcefully attaching socket to the port 9999 
+	if (bind(server_fd, (struct sockaddr *)&address, 
+								sizeof(address))<0) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	if (listen(server_fd, 3) < 0) 
+	{ 
+		perror("listen"); 
+		exit(EXIT_FAILURE); 
+	} 
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+					(socklen_t*)&addrlen))<0) 
+	{ 
+		perror("accept"); 
+		exit(EXIT_FAILURE); 
+	} 
+	valread = read( new_socket, buffer, 2048); 
+
+   if (buffer[0] == '1')
    {
-      printf("Not enough arguments provided.");
-      PrintUseCase();
+      FILE *fp = fopen(&buffer[2], "r+");
+      
+      fseek(fp, 0L, SEEK_END);
+      long size = ftell(fp);
+      rewind(fp);
+
+      char * data;
+      data = (char*) calloc(size, 1);
+      fread(data, 1, size, fp);
+      
+      fclose(fp);
+      uint8_t * encoded_data;
+      base64_encode(data, size, size, encoded_data);
+      free(data);
+      
+      fp = fopen(OutputFileForEncoded, "w+");
+      fwrite(encoded_data, 1, size, fp);
+      fclose(fp);
+
+      printf("Encoded Data is stored into: %s \n", OutputFileForEncoded);
    }
-   else if(argc == 3)
+   else if (buffer[0] == '2')
    {
-       if (*argv[1] == '1')
-       {
-            FILE *fp = fopen(argv[2], "r+");
-            
-            fseek(fp, 0L, SEEK_END);
-            long size = ftell(fp);
-            rewind(fp);
+      FILE *fp = fopen(&buffer[2], "r+");
+      
+      fseek(fp, 0L, SEEK_END);
+      long size = ftell(fp);
+      rewind(fp);
 
-            char * data;
-            fread(data, 1, size, fp);
-            
-            fclose(fp);
-            char * encoded_data = base64_encode(data, size, &size);
-            
-            fp = fopen(OutputFileForEncoded, "w+");
-            fwrite(encoded_data, 1, strlen(encoded_data), fp);
-            fclose(fp);
+      char * encoded_data;
+      encoded_data = (char*) calloc(size, 1);
+      fread(encoded_data, 1, size, fp);
+      
+      fclose(fp);
 
-            printf("Encoded Data is stored into: %s \n", OutputFileForEncoded);
-       }
-       else if (*argv[1] == '2')
-       {
-            FILE *fp = fopen(argv[2], "r+");
-            
-            fseek(fp, 0L, SEEK_END);
-            long size = ftell(fp);
-            rewind(fp);
+      char * decoded_data = base64_decode(encoded_data, size, &size);
+      free(encoded_data);
+      fp = fopen(OutputFileForDecoded, "w+");
+      fwrite(decoded_data, 1, size, fp);
+      fclose(fp);
 
-            char * encoded_data;
-            fread(encoded_data, 1, size, fp);
-            
-            fclose(fp);
-
-            char * decoded_data = base64_decode(encoded_data, size, &size);
-
-            fp = fopen(OutputFileForDecoded, "w+");
-            fwrite(encoded_data, 1, strlen(encoded_data), fp);
-            fclose(fp);
-
-            printf("Decoded Data is stored into: %s \n", OutputFileForDecoded);
-       }
-       else if (*argv[1] == '3')
-       {
-          system(argv[2]);
-       }
+      printf("Decoded Data is stored into: %s \n", OutputFileForDecoded);
    }
-   else if( argc > 3 )
+   else if (buffer[0] == '3')
    {
-      printf("Too many arguments supplied.\n");
-      PrintUseCase();
+      system(&buffer[2]);
    }
-   else
-   {
-      printf("Two arguments expected.\n");
-      PrintUseCase();
-   }
-
+   
 
 
    return 0;
